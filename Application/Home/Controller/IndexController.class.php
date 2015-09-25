2fc388ea9fd1ef62c12ee421d104ec03f71b5277
whitegraduate <zhushuliang@outlook.com>
2015-09-25 15:27:36 +0800

    test

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,25 +2,9 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-	public function __construct(){
-
-	}
-
     public function index(){
-    $nonce      =$_GET['nonce'];
-    $token	    ='weixin';
-    $timestamp  =$_GET['timestamp'];
-    $echostr	=$_GET['echostr'];
-    $signature  =$_GET['signature'];
-    $array		=array();
-    $array		=array($token,$timestamp,$nonce);
-    sort($array);
-    $str     	=sha1(implode($array));
-    if ($str   ==$signature) {
-    	echo $echostr;
-    	exit;
+        echo 'success';
     }
-    function show(){
-    	echo 'imooc';
-    }
+
 }
+
