d30db0e8395c8e2b83fd88d03db5505c3e2ee090
whitegraduate <zhushuliang@outlook.com>
2015-09-25 23:30:23 +0800

    test 3

--- index.php
+++ index.php
@@ -1,33 +1,4 @@
 <?php 
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
-
-
-/**
- * 系统调试设置
- * 项目正式部署后请设置为false
- */
-define('APP_DEBUG', true );
-
-/**
- * 应用目录设置
- * 安全期间，建议安装调试完成后移动到非WEB目录
- */
-define ( 'APP_PATH', './Application/' );
- 
-
-/**
- * 缓存目录设置
- * 此目录必须可写，建议移动到非WEB目录
- */
-define ( 'RUNTIME_PATH', './Runtime/' );
-
-
-/**
- * 引入核心入口
- * ThinkPHP亦可移动到WEB以外的目录
- */
-require './ThinkPHP/ThinkPHP.php';
-
 
 define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
@@ -43,7 +14,7 @@ class wechatCallbackapiTest
     {
         $echoStr = $_GET["echostr"];
         if($this->checkSignature()){
-            header('content-type:text');
+            header('content-type:text');i
             echo $echoStr;
             exit;
         }
@@ -99,4 +70,34 @@ class wechatCallbackapiTest
         }
     }
 }
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+
+/**
+ * 系统调试设置
+ * 项目正式部署后请设置为false
+ */
+define('APP_DEBUG', true );
+
+/**
+ * 应用目录设置
+ * 安全期间，建议安装调试完成后移动到非WEB目录
+ */
+define ( 'APP_PATH', './Application/' );
+ 
+
+/**
+ * 缓存目录设置
+ * 此目录必须可写，建议移动到非WEB目录
+ */
+define ( 'RUNTIME_PATH', './Runtime/' );
+
+
+/**
+ * 引入核心入口
+ * ThinkPHP亦可移动到WEB以外的目录
+ */
+require './ThinkPHP/ThinkPHP.php';
+
+
 ?>
